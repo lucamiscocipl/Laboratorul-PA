4d00769395c4017bc5c043a0072d6175b0c83e2e
luca miscoci <luca.miscoci@gmail.com>
2026-03-04 19:02:58 +0200

    v2

--- lab2.c
+++ lab2.c
@@ -2,16 +2,15 @@
 #include <stdio.h>
 
 int function(int x){
-	x=x-2;
-	x=function(x);
-	return x;
+    x -= 2;
+    return x;
 }
 
 int main(){
 	int a=30, i=0;
 	
 	while (a>0){
-		a=function(a);
+		a = function (a);
 		i++;
 	}
 	printf("%d", i);
